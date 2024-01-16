8d998eadb06db60db6ce7c3e0a41925ecbea60ec
Alessandro Sylar <lsn.lodigiani@gmail.com>
2024-01-16 12:39:46 +0100

    fix

--- 50-js-funzioni-2/main.js
+++ 50-js-funzioni-2/main.js
@@ -98,6 +98,8 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+
+
 Bonus
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
