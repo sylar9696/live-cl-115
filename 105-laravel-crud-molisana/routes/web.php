96d28c993cec62810c8b7d928dc4540b068b35bf
Alessandro Lodigiani <lsn.lodigiani@gmail.com>
2024-03-18 13:10:30 +0100

    crud

---
+++ 105-laravel-crud-molisana/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\backend\PastaController;
+use App\Http\Controllers\Guest\PageController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Home\WelcomeController as WelcomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', [PageController::class, 'index'])->name('homePage');
+
+Route::resource('pastas', PastaController::class );
+
+
